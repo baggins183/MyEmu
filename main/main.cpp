292de7a0dfa7235d159dd91fc4e916a279c5d465
fakename <fakeemail>
2023-02-19 20:21:17 -0800

    working on SceSysModule init function

--- main/main.cpp
+++ main/main.cpp
@@ -109,29 +109,50 @@ bool mapEntryModuleIntoMemory(fs::path nativeExecutablePath) {
     return true;
 }
 
-static bool doInit(std::string libName, InitFiniInfo &initFiniInfo) {
-    void *dl = dlopen(libName.c_str(), RTLD_LAZY);
+typedef int (*PFN_PS4_INIT_FUNC) (int, const char **, char **);
+
+struct Ps4InitFuncArgs {
+    int argc;
+    char **argv;
+    char **environ;
+};
+
+static bool callInitFunctions(std::string ps4Name, InitFiniInfo &initFiniInfo) {
+    bool success = true;
+    const char *argv[] = {
+        // TODO use .so name? Or entry name?
+        "eboot.bin"
+    };
+    int argc = 1;
+
+    std::string nativeName = getNativeLibName(fs::path(ps4Name).filename());
+
+    void *dl = dlopen(nativeName.c_str(), RTLD_LAZY);
     if ( !dl) {
+        success = false;
         goto error;
     }
     struct link_map *l;
-    if ( !dlinfo(dl, RTLD_DI_LINKMAP, &l)) {
+    if ( dlinfo(dl, RTLD_DI_LINKMAP, &l)) {
+        success = false;
         goto error;
-    }
+    }  
 
     if ( !initFiniInfo.dt_preinit_array.empty()) {
-
+        for (uint i = 0; i < initFiniInfo.dt_preinit_array.size(); i++) {
+            ((PFN_PS4_INIT_FUNC) (l->l_addr + initFiniInfo.dt_preinit_array[i])) (argc, argv, NULL);
+        }
     }
 
     if ( initFiniInfo.dt_init) {
-
+        ((PFN_PS4_INIT_FUNC) (l->l_addr + initFiniInfo.dt_init.value())) (argc, argv, NULL);
     }
 
     if ( !initFiniInfo.dt_init_array.empty()) {
-        
+        for (uint i = 0; i < initFiniInfo.dt_init_array.size(); i++) {
+            ((PFN_PS4_INIT_FUNC) (l->l_addr + initFiniInfo.dt_init_array[i])) (argc, argv, NULL);
+        }
     }
-
-    return true;
 
 error:
     char *err;
@@ -142,7 +163,7 @@ error:
     if (dl) {
         dlclose(dl);
     }
-    return false;
+    return success;
 }
 
 int main(int argc, char **argv) {
@@ -205,6 +226,9 @@ int main(int argc, char **argv) {
             if ( !patchPs4Lib(Ctx, nativePath)) {
                 return -1;
             }
+        } else {
+            // TODO read DT_NEEDED ents to build topological order of deps
+            // So we can call DT_INIT-like functions in correct order
         }
         std::string nativeName = getNativeLibName(libName.filename());
 
@@ -237,7 +261,7 @@ int main(int argc, char **argv) {
 
     // These may depend on ps4 libs (for wrapping sce functions by doing1 dlsym(RTLD_NEXT, ...))
     // So preload these only after patching dependencies
-    // Note RTLD_GLOBAL: these symbols will take precedence over ps4 symbols in relocations
+    // Note RTLD_GLOBAL: these symbols will take precedence over ps4 symbols in lookups/relocs
     for (std::string preload: Ctx.preloadNames) {
         auto preloadPath = preloadSearcher.findLibrary(preload);
         assert(preloadPath);
@@ -253,13 +277,8 @@ int main(int argc, char **argv) {
         preloadHandles.push_back(handle);
     }
 
-    //void *testdl = dlmopen(LM_ID_NEWLM, "./build/ps4lib_overloads/libevery.so", RTLD_NOW | RTLD_GLOBAL);
-
-
     // Load ps4 module
-    // dl = dlmopen(preloadNamespace, firstLib.c_str(), RTLD_LAZY);
     dl = dlopen(firstLib.c_str(), RTLD_LAZY);
-    //dl = dlmopen(lmid, firstLib.c_str(), RTLD_LAZY);
     if (!dl) {
         char *err;
         while ((err = dlerror())) {
@@ -268,9 +287,13 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    // do initialization for all libs, but not the entry module yet
-    for (uint i = worklist.size() - 1; i > 0; i++) {
-
+    // Do initialization for all libs, but not the entry module yet
+    // Do reverse order of deps
+    // TODO does preinit go in forward order?
+    //for (uint i = worklist.size() - 1; i > 0; i--) {
+    for (uint i = 1; i >= 0; i--) {
+        fs::path nativeLibName = getNativeLibName(worklist[i]);
+        callInitFunctions(worklist[i], initFiniInfos[nativeLibName]);
     }
 
     for (auto handle: preloadHandles) {
---
+++ ps4lib_overloads/SceLibKernel/sce_kernel.cpp
@@ -0,0 +1,63 @@
+#include "sce_errors/sce_errors.h"
+#include "Common.h"
+
+#include <signal.h>
+
+struct  sce_module_handle_t {
+    // Fairly confident
+    // code uses 4B regs to pass around
+    uint32_t handle;
+    // probably analagous to module id from TLS
+};
+
+// I'm not sure what bits are part of a request if any, and what parts of this struct are for filling in
+struct sce_module_info_request_t {
+    // Guessing
+    // movq used to setup contents, so assume 8B
+    uint64_t bits;
+
+    // some bits are probably for the compiled SDK version
+};
+
+extern "C" {
+
+// Note: I saw this crash on a code path through libSceSysModule's DT_INIT function
+// Implementing this leaves a crash in sceKernelGetModuleInfo
+sce_module_handle_t sceKernelGetExecutableModuleHandle(void) {
+    return { NULL };
+}
+
+// Defintely takes 2 args, not more.
+// Not sure what $2 is exactly.
+// Returns error code
+// TODO check where errors are coming from: break on printf, go down callstack to see what condition lead to error
+int sceKernelGetModuleInfo(sce_module_handle_t mod, sce_module_info_request_t *request) {
+    switch(request->bits) {
+        case 0x160:
+            // something to do with the SDK version
+            // TODO figure out where to write SDK version
+            
+            // Trace when returning non-zero:
+            //[Sysmodule(177726) 3607] sceKernelGetModuleInfo() fails (ffffffff)
+            //[Sysmodule(177726) 3122] get_process_info() fails (ffffffff)            
+
+            // Trace when returning 0:
+            // [Sysmodule(178214) 3133] Cannot get SDK version (0x80020016)
+            // dipsw [0x06]: open failed, err = 0x8001ffea            
+
+            return 0;
+        default:
+            return 0;
+    }
+}
+
+int sceKernelGetModuleInfo2(void) {
+    raise(SIGTRAP);
+    return 0;
+}
+
+int sceKernelGetCompiledSdkVersion(void) {
+    return 0;
+}
+
+}
